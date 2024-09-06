e5e50f011592329d5ad5a899d70b42854e47288d
Otmane <166559635+otboumeh@users.noreply.github.com>
2024-09-06 16:53:09 +0200

    to see

--- Test/src/ejercicionumerouno/ejemplo.java
+++ Test/src/ejercicionumerouno/ejemplo.java
@@ -6,21 +6,37 @@ public class ejemplo {
 
 	static Scanner teclado = new Scanner(System.in);
 
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 2;
-		int j = 1;
+		int i = 0;
+		int j = 0;
 		int resultado = 0;
-		//pedir numero 1
+		int restar = 0;
+		int multiplicar = 0;
+		int dividir = 0;
+		// pedir numero 1
 		System.out.print("Dame un numero : ");
 		i = teclado.nextInt();
-		//pedir numero 2
-		System.out.print("Dame otro numero : ");
+		// pedir numero 2
+		System.out.println("Dame otro numero : ");
 		j = teclado.nextInt();
 
-		 resultado = i + j;
-			System.out.print("el resultado es : " + resultado);		
+		resultado = i + j;
+		System.out.println("el resultado de sumar es : " + resultado);
+		restar = i - j;
+		System.out.println("el resultado de restar es : " + restar);
+		multiplicar = i * j;
+		System.out.println("el resultado de multiplicar es : " + multiplicar);
+		dividir = i / j;
+		if (j == 0);
+		{
+			System.out.println("no se puedo dividir en 0");
+		}
+		if (j != 0)
+		{	
+			System.out.println("el resultado de dividir es : " + dividir);
+
 	}
 
+}
 }
