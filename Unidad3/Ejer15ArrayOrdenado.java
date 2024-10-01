6f87086fa6436e638126585083468191d7dc29ca
Otmane <166559635+otboumeh@users.noreply.github.com>
2024-10-01 20:21:58 +0200

    Add files via upload

--- Unidad3/Ejer15ArrayOrdenado.java
+++ Unidad3/Ejer15ArrayOrdenado.java
@@ -1,10 +1,23 @@
 package Unidad3;
-
+import java.util.Scanner;
 public class Ejer15ArrayOrdenado {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		int numero =0;
+		int[] array = new int[50];
+		for(int i = 0; numero>0;i++) {
+			System.out.println("Give me a positif number");
+			
+			numero = teclado.nextInt();
+			if (numero >0) {
+				array[i]= numero;
+			}
+			else {
+				System.out.println("Sorry the number is negatif");
+			}
+		}
+		teclado.close();
 	}
 
 }
